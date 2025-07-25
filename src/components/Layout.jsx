ec30f73e761d62b8379e78fb69aa82b3aa28770f
Ali Arshad Khan <aliarshadkhan02@gmail.com>
2025-07-26 02:40:52 +0530

    added footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom"
 import Header from "./Header"
+import Footer from "./Footer"
 export default function layout() {
     return (
-        <>
+        <div className="site-wrapper">
         <Header />
-        <Outlet />
-        </>
+         <main>
+            <Outlet />
+         </main>
+         <Footer />
+        </div>
     )
 }
