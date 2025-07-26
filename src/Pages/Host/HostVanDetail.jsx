0700f794575c136c4d45acc7c40c2e1e0edce1e6
Ali Arshad Khan <aliarshadkhan02@gmail.com>
2025-07-26 17:49:02 +0530

    created vanDetail page

--- src/Pages/Host/HostVanDetail.jsx
+++ src/Pages/Host/HostVanDetail.jsx
@@ -1,5 +1,30 @@
+import {useEffect, useState} from "react"
+import {useParams} from "react-router-dom"
+
 export default function HostVanDetail() {
+    const params = useParams();
+    const [van, setVan] = useState(null);
+    
+    useEffect(() => {
+        fetch(`/api/host/vans/${params.id}`)
+        .then(res => res.json())
+        .then(data => setVan(data.vans[0]))
+    },[])
+    
+      if (!van) {
+        return <h1>Loading...</h1>
+    }
+
     return (
-        <h1>Host vans detail page goes here</h1>
+        <section>
+        <div className = "hostVanDetail-container">
+        <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+                <div className="host-van-info">
+                <i className={`van-type van-type-${van.type}`}>{van.type}</i>
+                    <h3>{van.name}</h3>
+                    <p><span>${van.price}</span>/day</p>
+                </div>
+        </div>
+        </section>
     )
 }
