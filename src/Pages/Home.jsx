2987329432b674f82a3f150bc9684f8ba114acc5
Ali Arshad Khan <aliarshadkhan02@gmail.com>
2025-07-30 20:18:52 +0530

    customize hero section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,21 @@
 import {Link} from "react-router-dom";
+import pexel from "../assets/pexel.jpg"
+import pexel2 from "../assets/pexel2.jpg"
 export default function Home() {
     return (
         <>
         <div className="hero-section">
+            <div className="hero-section-top">
             <h1>You got the travel plans, we got the travel vans.</h1>
             <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
             <Link to="vans">Find your van</Link>
+            </div>
+            <div className="hero-image">
+                <img src={pexel} alt="" />
+                <img src={pexel2} alt="" />
+            </div>
+           
+
         </div>
         </>
     )
--- src/Pages/Host/HostVanDetail.jsx
+++ src/Pages/Host/HostVanDetail.jsx
@@ -39,7 +39,7 @@ export default function HostVanDetail() {
     }
 
     return (
-        <section>
+        <section className="hostVanDetail-page">
          <NavLink
             to=".."
             relative="path"
