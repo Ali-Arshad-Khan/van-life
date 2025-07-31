67a5288cfd910b8fcb3ea319c66fe97ec32ad4fd
Ali Arshad Khan <aliarshadkhan02@gmail.com>
2025-07-31 15:27:40 +0530

    hide api js due to github secret revealed issues

--- src/api.js
+++ src/api.js
@@ -10,12 +10,12 @@ import {
 } from "firebase/firestore/lite";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCdalNJw5KoKDUBijdhUp--Ba0NBO8Lk40",
+  authDomain: "vanlife-13aba.firebaseapp.com",
+  projectId: "vanlife-13aba",
+  storageBucket: "vanlife-13aba.firebasestorage.app",
+  messagingSenderId: "890495797042",
+  appId: "1:890495797042:web:68cbf4ed093e5ac7d0392b"
 };
 
 export const app = initializeApp(firebaseConfig);
